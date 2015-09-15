09b60dd865e989694bcbd9c623cb70fc74607419
faizankhan29 <faizankh29@gmail.com>
2015-09-15 19:54:54 +0530

    Added prequestions.ejs and questions json

---
+++ models/questions.js
@@ -0,0 +1,17 @@
+[{
+    "Number":"1",
+    "Question":"What is your name?",
+"Anwer":"Abc"},{
+    "Number":"2",
+    "Question":"What is your branch?",
+"Anwer":"Abc"},{
+    "Number":"3",
+    "Question":"What is your registration number?",
+"Anwer":"Abc"},{
+    "Number":"4",
+    "Question":"What is your age?",
+"Anwer":"Abc"},{
+    "Number":"5",
+    "Question":"What is your hometown?",
+"Anwer":"Abc"}]
+ 
